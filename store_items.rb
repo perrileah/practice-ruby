c934f93bb810da7b60ecc88b4f83fab5409c1ceb
Leah Perri <perrileah2023@gmail.com>
2023-02-21 14:20:29 -0600

    items represented as a hash

--- store_items.rb
+++ store_items.rb
@@ -6,3 +6,10 @@ item3 = ["celery", 3.00, "in-store"]
 
 puts "The #{item1[0]} costs #{item1[1]} and is available #{item1[2]}"
 
+# items represented as a hash
+
+item1 = {"type" => "carrot", "cost" => 1.00, "availability" => "in-store"}
+item2 = {"type" => "broccoli", "cost" => 2.00, "availability" => "online-only"}
+item3 = {"type" => "celery", "cost" => 3.00, "availability" => "in-store"}
+
+#items represented as a hash with symbols
