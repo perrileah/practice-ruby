dcb4cf6135e86d0e9bffe212d1813839ca3617fd
Leah Perri <perrileah2023@gmail.com>
2023-02-21 14:14:40 -0600

    init

---
+++ store_items.rb
@@ -0,0 +1,8 @@
+# items represented as an array 
+
+item1 = ["carrot", 1.00, "in-store"]
+item2 = ["broccoli", 2.00, "online-only"]
+item3 = ["celery", 3.00, "in-store"]
+
+puts "The #{item1[0]} costs #{item1[1]} and is available #{item1[2]}"
+
