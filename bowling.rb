85b347c90bba69a46eea682a858b05e5e034b4af
Leah Perri <perrileah2023@gmail.com>
2023-03-02 10:54:16 -0600

    bowling progress!

--- bowling.rb
+++ bowling.rb
@@ -3,22 +3,35 @@
 total_score = []
 
 
-def roll
-  roll = rand(0..10)
+def roll(turn)
+  if turn == 1
+    roll = rand(0..10)
+    @pins_remaining = 10 - roll
+    return roll
+    # p "Pins remaining after roll one is #{@pins_remaining}"
+  elsif turn == 2
+    p "Pins remaining after roll one is #{@pins_remaining}"
+    roll = @pins_remaining - rand(0..@pins_remaining)
+  end
 end
 
-turn_one = roll
-pins_remaining = 10 - turn_one
+
+
+turn_one = roll(1)
+turn_two = roll(2)
 
 total_score << turn_one
+total_score << turn_two
 
 
-p "The roll is #{turn_one}"
-p "You have #{pins_remaining} pins remaining"
+p "Turn one is #{turn_one}"
+p "You have #{@pins_remaining} pins remaining"
+p "Turn two is #{turn_two}"
+p "You have #{@pins_remaining} pins remaining"
 p "The total score is #{total_score}"
 
 
-if pins_remaining == 0
+if @pins_remaining == 0
   puts "Congrats! You got a strike!"
 else
   puts "Take another turn!"
